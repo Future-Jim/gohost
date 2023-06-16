c454bfe506dceb0c6274894a2108a7250ec985c1
James Arias <jamesarias@protonmail.com>
2023-06-16 16:16:48 -0400

    updating metric testing

--- gohost-mon/metrics_test.go
+++ gohost-mon/metrics_test.go
@@ -3,31 +3,41 @@ package main
 import (
 	"reflect"
 	"testing"
-
-	"github.com/future-jim/gohost/lib/types"
 )
 
 func TestGetMemoryPercentUsed(t *testing.T) {
 	got := GetMemoryPercentUsed()
-	want := new(types.PercentMemoryUsed)
-
-	if reflect.TypeOf(&got) != reflect.TypeOf(want) {
-		t.Error("did not return a struct of type", reflect.TypeOf(want))
+	//dumby int to test against return type in GetMemoryPercentUsed
+	var want int
+	if reflect.TypeOf(got.PMU) != reflect.TypeOf(want) {
+		t.Error("Did not return a value of type", reflect.TypeOf(want))
 	}
 }
 
 func TestGetAverageLoad(t *testing.T) {
 	got := GetAverageLoad()
-	want := new(types.AverageLoad)
-	if reflect.TypeOf(&got) != reflect.TypeOf(want) {
-		t.Error("did not return a struct of type", reflect.TypeOf(want))
+	var want float64
+	if reflect.TypeOf(got.One) != reflect.TypeOf(want) {
+		t.Error("load_1 returned incorrect type => ", reflect.TypeOf(got.One))
+	}
+	if reflect.TypeOf(got.Five) != reflect.TypeOf(want) {
+		t.Error("load_1 returned incorrect type =>", reflect.TypeOf(want))
+	}
+	if reflect.TypeOf(got.Fifteen) != reflect.TypeOf(want) {
+		t.Error("load_1 returned incorrect type =>", reflect.TypeOf(want))
 	}
 }
 
 func TestGetHostUpTime(t *testing.T) {
 	got := GetHostUpTime()
-	want := new(types.HostUpTime)
-	if reflect.TypeOf(&got) != reflect.TypeOf(want) {
-		t.Error("did not return a struct of type", reflect.TypeOf(want))
+	var want uint64
+	if reflect.TypeOf(got.Days) != reflect.TypeOf(want) {
+		t.Error("GetHostUpTime returned incorrect type for day:", reflect.TypeOf(got.Days))
+	}
+	if reflect.TypeOf(got.Hours) != reflect.TypeOf(want) {
+		t.Error("GetHostUpTime returned incorrect type for day:", reflect.TypeOf(got.Hours))
+	}
+	if reflect.TypeOf(got.Minutes) != reflect.TypeOf(want) {
+		t.Error("GetHostUpTime returned incorrect type for day:", reflect.TypeOf(got.Minutes))
 	}
 }
