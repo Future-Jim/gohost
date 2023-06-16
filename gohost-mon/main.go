dcee7b7c1cf71296bf48a3592864196f4aa6a640
James Arias <jamesarias@protonmail.com>
2023-06-16 13:30:01 -0400

    updating main to insert into db

--- gohost-mon/main.go
+++ gohost-mon/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"time"
 
-	storage "github.com/future-jim/gohost-storage"
+	"github.com/future-jim/gohost/lib/storage"
+	"github.com/future-jim/gohost/lib/types"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/load"
 )
 
-func main() {
-	var metric Metrics
-	storage.NewPostgresStore()
+const bytesToMB = 1048576
+const secondsInHour = 3600
+const secondsInDay = 86400
+const measurementDelay = 60
 
+func main() {
+	var metric types.Metrics
+	store, err := storage.NewPostgresStore()
+	if err != nil {
+		return
+	}
+	store.Init()
 	for {
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(measurementDelay * time.Second)
 		PMU := getMemoryPercentUsed()
 		AL := getAverageLoad()
 		HUT := getHostUpTime()
@@ -24,34 +33,38 @@ func main() {
 		metric.AL = AL
 		metric.PMU = PMU
 		metric.HUT = HUT
-		fmt.Println(metric)
+		store.AddEntry(&metric)
 
 	}
 }
 
-func getMemoryPercentUsed() PercentMemoryUsed {
+func getMemoryPercentUsed() types.PercentMemoryUsed {
 	v, _ := mem.VirtualMemory()
-	return PercentMemoryUsed{
-		PMU: v.UsedPercent,
+	return types.PercentMemoryUsed{
+		PMU: int(math.Trunc(v.UsedPercent)),
 	}
 }
-func getHostUpTime() HostUpTime {
+func getHostUpTime() types.HostUpTime {
 	h, _ := host.Info()
 	days, _ := divmod(h.Uptime, secondsInDay)
 	hours, minutes := divmod(h.Uptime, secondsInHour)
-	return HostUpTime{
+	return types.HostUpTime{
 		Days:    days,
 		Hours:   hours,
 		Minutes: minutes / 60,
 	}
 }
 
-func getAverageLoad() AverageLoad {
+func getAverageLoad() types.AverageLoad {
 	l, _ := load.Avg()
-	return AverageLoad{
-		One:     l.Load1,
-		Five:    l.Load5,
-		Fifteen: l.Load15,
+	var l1 float64 = roundFloat(l.Load1, 2)
+	var l5 float64 = roundFloat(l.Load5, 2)
+	var l15 float64 = roundFloat(l.Load15, 2)
+
+	return types.AverageLoad{
+		One:     l1,
+		Five:    l5,
+		Fifteen: l15,
 	}
 }
 
@@ -59,4 +72,9 @@ func divmod(numerator, denominator uint64) (quotient, remainder uint64) {
 	quotient = numerator / denominator
 	remainder = numerator % denominator
 	return quotient, remainder
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
